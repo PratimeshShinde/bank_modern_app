21c991d3b81cbf38bc561cde05e8f82cb2b831b3
Prati <pratimeshshinde121@gmail.com>
2022-08-21 12:51:23 +0530

    first commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+import { close, logo, menu } from "../assets";
+
+import { navLinks } from "../constants";
+
+const CardDeal = () => {
+  return <div>CardDeal</div>;
+};
+
+export default CardDeal;
